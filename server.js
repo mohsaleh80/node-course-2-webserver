fce520bf189910bf2e0fc6aab1d6529828c02588
Mohammad Saleh <mohsaleh.raddad80@gmail.com>
2017-02-01 20:39:09 +0200

    update

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req,res,next)=>{
 
     fs.appendFile('log-server.log', logMessage, (err) => {
         if (err) {
-            console.log('The "data to append was not appended to file!');
+            console.log('The data to append was not appended to file!');
         }
     });
     next();
